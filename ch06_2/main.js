9d1a1680b8852ffa2b815db5bede3b263c503dab
Lee Ji Heok <olzlgur@naver.com>
2021-11-10 22:47:08 +0900

    2021-11-10

---
+++ ch06_2/main.js
@@ -0,0 +1,7 @@
+const decamelize = require('decamelize')
+console.log(decamelize('unicornRainbow')) // 스네이크 케이스로 바꿔줌
+
+// http://example.com/index.html
+// http://example.com/
+// 동일한 취급
+// 웹에서는 인덱스라는 파일은 디렉토리의 내용물을 보여주는 역할을 함.
